5f018f06918289ef710721d8eafc3bb20a07538a
hmazopacheco149 <hmazopacheco149@gmail.com>
2025-07-12 23:05:12 +0000

    Entrega

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,6 +2,8 @@
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
 
+import pandas as pd
+import os
 
 def pregunta_01():
     """
@@ -13,3 +15,31 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+
+    import pandas as pd
+    import os
+    df = pd.read_csv("files/input/solicitudes_de_credito.csv", sep=";", index_col=0)
+    
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower().str.strip()
+    df["idea_negocio"] = df["idea_negocio"].str.lower().str.replace("_", " ").str.replace("-", " ").str.strip()
+    df["barrio"] = df["barrio"].str.lower().str.replace("_", " ").str.replace("-", " ")
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df["monto_del_credito"] = df["monto_del_credito"].str.strip().str.replace("$", "").str.replace(",", "").str.replace(".00", "").astype(int)
+    df["línea_credito"] = df["línea_credito"].str.lower().str.strip().str.replace("_", " ").str.replace("-", " ").str.strip()
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df["fecha_de_beneficio"] = pd.to_datetime(
+        df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+    ).combine_first(
+        pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce")
+    )
+
+    df = df.drop_duplicates()
+    df = df.dropna()
+    
+
+    if not os.path.exists("files/output"):
+        os.makedirs("files/output")
+    df.to_csv("files/output/solicitudes_de_credito.csv", sep=';', index=False)
+
+    return 0
